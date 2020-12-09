2eeb3007f4e1571dce21391daae4ef4fc3e0baa6
supremest35 <supremest.35@gmail.com>
2020-12-09 19:45:59 +0900

    ksyweb 프로젝트 #2. 유틸 구성

---
+++ ksyweb/src/kr/co/ksy/web/util/ConnectionUtil.java
@@ -0,0 +1,25 @@
+package kr.co.ksy.web.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+
+	private static final String DRIVER_NAME = "oracle.jdbc.OracleDriver";
+	private static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String USER_NAME = "hta";
+	private static final String USER_PASSWORD = "zxcv1234";
+	
+	static {
+		try {
+			Class.forName(DRIVER_NAME);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(DB_URL, USER_NAME, USER_PASSWORD);
+	}
+}
